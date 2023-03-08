96536e6b183e81d618a0d1e0d5c8bc26ff21400e
+ <+>
2023-03-07 21:01:47 -0500

    commit tree 5e9b4f8422c4f40bd0be82aa2650f6d6affcc352

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line 
+    if katz_deli.length == 0
+        "The line is currently empty."
+    else 
+        katz_deli.map.with_index {|value, index|
+            "The line is currently: #{index}. #{value} "
+        }
+    end
+end
+
